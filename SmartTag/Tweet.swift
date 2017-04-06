9fd07ca5124854b8b8e3ab161e74dcf995884118
ThangTruong <thangtm6@fsoft.com.vn>
2017-04-06 07:09:06 +0700

    Xây dựng khung sườn của project và tweetTableViewController

---
+++ SmartTag/Tweet.swift
@@ -0,0 +1,13 @@
+//
+//  Tweet.swift
+//  SmartTag
+//
+//  Created by Trương Thắng on 4/6/17.
+//  Copyright © 2017 Trương Thắng. All rights reserved.
+//
+
+import Foundation
+
+class Tweet {
+    
+}
